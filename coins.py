83dc8c74fa61e62522fb9bc07b08422b8bbe6ff9
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-06-18 21:03:18 -0400

    Implement str and immutability on coins

--- coins.py
+++ coins.py
@@ -3,27 +3,40 @@ Stores coin definitions
 """
 from dataclasses import dataclass
 
+@dataclass(frozen=True)
+class Coin:
+    value: int
+    label: str
+
+    def __str__(self):
+        return self.label
+
 
 @dataclass(frozen=True)
-class Nickel:
+class Nickel(Coin):
     value: int = 5
+    label: str = '5¢'
 
 
 @dataclass(frozen=True)
-class Dime:
+class Dime(Coin):
     value: int = 10
+    label: str = '10¢'
 
 
 @dataclass(frozen=True)
-class Quarter:
+class Quarter(Coin):
     value: int = 25
+    label: str = '25¢'
 
 
 @dataclass(frozen=True)
-class Loonie:
+class Loonie(Coin):
     value: int = 100
+    label: str = '$1'
 
 
 @dataclass(frozen=True)
-class Toonie:
+class Toonie(Coin):
     value: int = 200
+    label: str = '$2'
