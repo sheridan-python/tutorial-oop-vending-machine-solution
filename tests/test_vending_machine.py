ff0a268e73ade6d8a5ed5905dfd76685a5933481
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-06-19 09:05:02 -0400

    Implement buy_product method validation to accept Product subclasses

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,6 +1,7 @@
 import pytest
 
 import coins
+import products
 from vending_machine import VendingMachine
 
 
@@ -28,8 +29,29 @@ def test_inserted_coins_are_appended_to_coin_list():
     machine.insert_coin(coins.Toonie())
     machine.insert_coin(coins.Quarter())
 
-    assert machine.coins == [
+    assert machine.inserted_coins == [
         coins.Loonie(),
         coins.Toonie(),
         coins.Quarter()
     ]
+
+
+def test_buy_product_rejects_strings():
+    machine = VendingMachine()
+
+    with pytest.raises(ValueError):
+        machine.buy_product(str)
+
+def test_buy_product_accepts_chips():
+    machine = VendingMachine()
+    machine.buy_product(products.Chips)
+
+
+def test_buy_product_accepts_drink():
+    machine = VendingMachine()
+    machine.buy_product(products.Drink)
+
+
+def test_buy_product_accepts_candy():
+    machine = VendingMachine()
+    machine.buy_product(products.Candy)
--- vending_machine.py
+++ vending_machine.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass, field
 
 import coins
+import products
 
 
 @dataclass
 class VendingMachine:
-    coins: list = field(default_factory=list)
+    inserted_coins: list = field(default_factory=list)
 
     def insert_coin(self, coin):
         if not isinstance(coin, coins.Coin):
             raise ValueError
 
-        self.coins.append(coin)
+        self.inserted_coins.append(coin)
+
+    def buy_product(self, product):
+        if not issubclass(product, products.Product):
+            raise ValueError
