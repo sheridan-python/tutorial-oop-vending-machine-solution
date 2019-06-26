d71904ed975b37c4535be649ed38c124e77fe558
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-06-25 21:27:36 -0400

    Implement buy_product and get_balance

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -2,7 +2,7 @@ import pytest
 
 import coins
 import products
-from vending_machine import VendingMachine
+from vending_machine import InsufficientFunds, VendingMachine
 
 
 def test_insert_coin_rejects_integer():
@@ -42,16 +42,60 @@ def test_buy_product_rejects_strings():
     with pytest.raises(ValueError):
         machine.buy_product(str)
 
-def test_buy_product_accepts_chips():
+
+def test_buy_product_accepts_chips_with_sufficient_balance():
     machine = VendingMachine()
+    machine.insert_coin(coins.Toonie())
+    machine.insert_coin(coins.Quarter())
     machine.buy_product(products.Chips)
 
 
 def test_buy_product_accepts_drink():
     machine = VendingMachine()
+    machine.insert_coin(coins.Toonie())
+    machine.insert_coin(coins.Loonie())
     machine.buy_product(products.Drink)
 
 
 def test_buy_product_accepts_candy():
     machine = VendingMachine()
+    machine.insert_coin(coins.Toonie())
+    machine.insert_coin(coins.Toonie())
     machine.buy_product(products.Candy)
+
+
+def test_get_balance_returns_zero_when_no_coins():
+    machine = VendingMachine()
+    assert machine.get_balance() == 0
+
+
+def test_get_balance_returns_sum_value_of_coins():
+    machine = VendingMachine()
+    machine.insert_coin(coins.Quarter())
+    machine.insert_coin(coins.Loonie())
+
+    assert machine.get_balance() == 125
+
+
+def test_buy_drink_with_insufficient_funds():
+    machine = VendingMachine()
+    with pytest.raises(InsufficientFunds):
+        machine.buy_product(products.Drink)
+
+
+def test_buy_product_returns_instance_when_valid():
+    machine = VendingMachine()
+    machine.insert_coin(coins.Toonie())
+    machine.insert_coin(coins.Toonie())
+    result = machine.buy_product(products.Drink)
+
+    assert isinstance(result, products.Drink)
+
+
+def test_get_balance_returns_sum_of_coins_minus_purchases():
+    machine = VendingMachine()
+    machine.insert_coin(coins.Toonie())
+    machine.insert_coin(coins.Toonie())
+    machine.buy_product(products.Candy)
+
+    assert machine.get_balance() == 85
--- vending_machine.py
+++ vending_machine.py
@@ -4,9 +4,14 @@ import coins
 import products
 
 
+class InsufficientFunds(Exception):
+    pass
+
+
 @dataclass
 class VendingMachine:
     inserted_coins: list = field(default_factory=list)
+    purchases: list = field(default_factory=list)
 
     def insert_coin(self, coin):
         if not isinstance(coin, coins.Coin):
@@ -17,3 +22,20 @@ class VendingMachine:
     def buy_product(self, product):
         if not issubclass(product, products.Product):
             raise ValueError
+        balance = self.get_balance()
+        if balance < product.price:
+            raise InsufficientFunds
+
+        obj = product()
+        self.purchases.append(obj)
+        return obj
+
+    def get_balance(self):
+        balance = 0
+        for coin in self.inserted_coins:
+            balance += coin.value
+
+        for product in self.purchases:
+            balance -= product.price
+
+        return balance
